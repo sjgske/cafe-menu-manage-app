f6d2f7ed50d73ca387317305329f4880c4c0df58
danheon kim <eksgjs3847@naver.com>
2022-02-23 02:22:58 +0900

    메뉴 추가 기능 완료

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,38 @@
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
-// 메뉴의 이름을 입력 받고 엔터키 입력으로 메뉴가 추가된다.
+// 메뉴의 이름을 입력 받고 확인 버튼, 엔터키 입력으로 메뉴가 추가된다.
 // 추가되는 메뉴의 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 // 총 메뉴 갯수를 count하여 상단에 보여준다.
 // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 const $ = (selector) => document.querySelector(selector);
+let menuCount = 0;
 
 function App() {
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    const espressoMenuName = $('#espresso-menu-name').value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return `<li class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-      >
-        수정
-      </button>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-      >
-        삭제
-      </button>
+    const menuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = `<li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${menuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
     </li>`;
-    };
+
+    if (menuName === '') {
+      alert('메뉴를 입력해주세요.');
+      return;
+    }
 
     // <!-- beforebegin -->
     // <ul>
@@ -37,8 +41,11 @@ function App() {
     // <!-- beforeend -->
     // </ul>
     // <!-- afterend -->
-    $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName))
+    $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate)
     $('#espresso-menu-name').value = '';
+
+    menuCount++;
+    $('.menu-count').innerText = `총 ${menuCount}개`;
   });
 }
 
