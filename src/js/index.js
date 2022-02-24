40cdfbf21bfc9dc1374b76b5200519cf4bc7e4be
danheon kim <eksgjs3847@naver.com>
2022-02-25 01:08:20 +0900

    Store menu items in LocalStorage & render menu items

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,49 @@
-// step1 요구사항 구현을 위한 전략
-// TODO 메뉴 추가
-// 메뉴의 이름을 입력 받고 확인 버튼, 엔터키 입력으로 메뉴가 추가된다.
-// 추가되는 메뉴의 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
-// 총 메뉴 갯수를 count하여 상단에 보여준다.
-// 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// 사용자 입력값이 빈 값이라면 추가되지 않는다.
-
-// TODO 메뉴 수정
-// 메뉴의 수정 버튼을 누르면 prompt 창이 뜬다.
-// 신규 메뉴명을 입력받고 확인 버튼을 누르면 메뉴 이름이 수정된다.
-
-// TODO 메뉴 삭제
-// 메뉴 삭제 버튼을 누르면 confirm 창이 뜬다.
-// 확인 버튼을 누르면 메뉴가 삭제된다.
-
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    return JSON.parse(localStorage.getItem("menu"));
+  }
+}
+
 function App() {
+  // 상태는 변하는 데이터! (메뉴명)
+  this.menu = [];
+  this.init = () => {
+    if (store.getLocalStorage().length > 1) {
+      this.menu = store.getLocalStorage();
+    }
+    render();
+  }
+
+  const render = () => {
+    const template = this.menu.map((menuItem, index) => {
+      return `
+        <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+        </li>`;
+    })
+      .join('');
+
+    $('#espresso-menu-list').innerHTML = template;
+    updateMenuCount();
+  }
+
   const updateMenuCount = () => {
     const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
     $('.menu-count').innerText = `총 ${menuCount}개`;
@@ -24,43 +51,23 @@ function App() {
 
   const addMenuName = () => {
     const menuName = $('#espresso-menu-name').value;
-    const menuItemTemplate = `<li class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${menuName}</span>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-    >
-      수정
-    </button>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-    >
-      삭제
-    </button>
-    </li>`;
+    this.menu.push({ name: menuName });
+    store.setLocalStorage(this.menu);
 
     if (menuName === '') {
       alert('메뉴명을 입력해주세요.');
       return;
     }
-
-    // <!-- beforebegin -->
-    // <ul>
-    // <!-- afterbegin -->
-    // <li></li>
-    // <!-- beforeend -->
-    // </ul>
-    // <!-- afterend -->
-    $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate)
+    render();
     $('#espresso-menu-name').value = '';
-
-    updateMenuCount();
   };
 
   const updateMenuName = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
     const $menuName = e.target.closest('li').querySelector(".menu-name");
     const newMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
+    this.menu[menuId].name = newMenuName;
+    store.setLocalStorage(this.menu);
 
     if (newMenuName === '') {
       alert('메뉴명을 입력해주세요!');
@@ -71,6 +78,9 @@ function App() {
 
   const removeMenuName = (e) => {
     if (confirm("정말 삭제하시겠습니까?")) {
+      const menuId = e.target.closest('li').dataset.menuId;
+      this.menu.splice(menuId, 1);
+      store.setLocalStorage(this.menu);
       e.target.closest('li').remove();
       updateMenuCount();
     }
@@ -91,4 +101,5 @@ function App() {
   });
 }
 
-App();
+const app = new App();
+app.init();
