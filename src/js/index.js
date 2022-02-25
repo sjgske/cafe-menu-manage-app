189e210b8e067508b1767d265ca8aca09c5c41e9
danheon kim <eksgjs3847@naver.com>
2022-02-26 02:21:16 +0900

    State handling on category-menu, Sold-out-menu handling

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,45 @@
-const $ = (selector) => document.querySelector(selector);
+const $ = selector => document.querySelector(selector);
 
 const store = {
-  setLocalStorage(menu) {
-    localStorage.setItem("menu", JSON.stringify(menu));
-  },
-  getLocalStorage() {
-    return JSON.parse(localStorage.getItem("menu"));
-  }
-}
+	setLocalStorage(menu) {
+		localStorage.setItem("menu", JSON.stringify(menu));
+	},
+	getLocalStorage() {
+		return JSON.parse(localStorage.getItem("menu"));
+	},
+};
 
 function App() {
-  // 상태는 변하는 데이터! (메뉴명)
-  this.menu = [];
-  this.init = () => {
-    if (store.getLocalStorage().length > 1) {
-      this.menu = store.getLocalStorage();
-    }
-    render();
-  }
+	// 상태는 변하는 데이터! (메뉴명)
+	this.menu = {
+		espresso: [],
+		frappuccino: [],
+		blended: [],
+		teavana: [],
+		desert: [],
+	};
+	this.currentCategory = "espresso";
+	this.init = () => {
+		if (store.getLocalStorage()) {
+			this.menu = store.getLocalStorage();
+		}
+		render();
+	};
 
-  const render = () => {
-    const template = this.menu.map((menuItem, index) => {
-      return `
+	const render = () => {
+		const template = this.menu[this.currentCategory]
+			.map((menuItem, index) => {
+				return `
         <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+        <span class="w-100 pl-2 menu-name ${menuItem.soldOut ? "sold-out" : ""} ">${
+					menuItem.name
+				}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+        >
+          품절
+        </button>
         <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -37,68 +53,92 @@ function App() {
           삭제
         </button>
         </li>`;
-    })
-      .join('');
+			})
+			.join("");
 
-    $('#espresso-menu-list').innerHTML = template;
-    updateMenuCount();
-  }
+		$("#menu-list").innerHTML = template;
+		updateMenuCount();
+	};
 
-  const updateMenuCount = () => {
-    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount}개`;
-  };
+	const updateMenuCount = () => {
+		const menuCount = $("#menu-list").querySelectorAll("li").length;
+		$(".menu-count").innerText = `총 ${menuCount}개`;
+	};
 
-  const addMenuName = () => {
-    const menuName = $('#espresso-menu-name').value;
-    this.menu.push({ name: menuName });
-    store.setLocalStorage(this.menu);
+	const addMenuName = () => {
+		const menuName = $("#menu-name").value;
+		this.menu[this.currentCategory].push({ name: menuName });
+		store.setLocalStorage(this.menu);
 
-    if (menuName === '') {
-      alert('메뉴명을 입력해주세요.');
-      return;
-    }
-    render();
-    $('#espresso-menu-name').value = '';
-  };
+		if (menuName === "") {
+			alert("메뉴명을 입력해주세요.");
+			return;
+		}
+		render();
+		$("#menu-name").value = "";
+	};
 
-  const updateMenuName = (e) => {
-    const menuId = e.target.closest('li').dataset.menuId;
-    const $menuName = e.target.closest('li').querySelector(".menu-name");
-    const newMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
-    this.menu[menuId].name = newMenuName;
-    store.setLocalStorage(this.menu);
+	const updateMenuName = e => {
+		const menuId = e.target.closest("li").dataset.menuId;
+		const $menuName = e.target.closest("li").querySelector(".menu-name");
+		const newMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
+		this.menu[this.currentCategory][menuId].name = newMenuName;
+		store.setLocalStorage(this.menu);
 
-    if (newMenuName === '') {
-      alert('메뉴명을 입력해주세요!');
-    } else if (newMenuName !== null) {
-      $menuName.innerText = newMenuName;
-    }
-  };
+		if (newMenuName === "") {
+			alert("메뉴명을 입력해주세요!");
+		} else if (newMenuName !== null) {
+			$menuName.innerText = newMenuName;
+		}
+	};
 
-  const removeMenuName = (e) => {
-    if (confirm("정말 삭제하시겠습니까?")) {
-      const menuId = e.target.closest('li').dataset.menuId;
-      this.menu.splice(menuId, 1);
-      store.setLocalStorage(this.menu);
-      e.target.closest('li').remove();
-      updateMenuCount();
-    }
-  };
+	const removeMenuName = e => {
+		if (confirm("정말 삭제하시겠습니까?")) {
+			const menuId = e.target.closest("li").dataset.menuId;
+			this.menu[this.currentCategory].splice(menuId, 1);
+			store.setLocalStorage(this.menu);
+			e.target.closest("li").remove();
+			updateMenuCount();
+		}
+	};
 
-  $('#espresso-menu-list').addEventListener('click', e => {
-    if (e.target.classList.contains('menu-edit-button')) {
-      updateMenuName(e);
-    }
-    if (e.target.classList.contains('menu-remove-button')) {
-      removeMenuName(e);
-    }
-  });
+	const soldOutMenu = e => {
+		const menuId = e.target.closest("li").dataset.menuId;
+		this.menu[this.currentCategory][menuId].soldOut =
+			!this.menu[this.currentCategory][menuId].soldOut;
+		store.setLocalStorage(this.menu);
+		render();
+	};
 
-  $('#espresso-menu-form').addEventListener('submit', e => {
-    e.preventDefault();
-    addMenuName();
-  });
+	$("#menu-list").addEventListener("click", e => {
+		if (e.target.classList.contains("menu-edit-button")) {
+			updateMenuName(e);
+			return;
+		}
+		if (e.target.classList.contains("menu-remove-button")) {
+			removeMenuName(e);
+			return;
+		}
+		if (e.target.classList.contains("menu-sold-out-button")) {
+			soldOutMenu(e);
+			return;
+		}
+	});
+
+	$("#menu-form").addEventListener("submit", e => {
+		e.preventDefault();
+		addMenuName();
+	});
+
+	$("nav").addEventListener("click", e => {
+		const isCategoryButton = e.target.classList.contains("cafe-category-name");
+		if (isCategoryButton) {
+			const categoryName = e.target.dataset.categoryName;
+			this.currentCategory = categoryName;
+			$("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+			render();
+		}
+	});
 }
 
 const app = new App();
